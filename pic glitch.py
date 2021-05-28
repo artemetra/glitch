29a0b46ba48c8a329ff2c1b2a78f3f41b08a6829
artemetra <artemlukin15@gmail.com>
2021-05-28 22:31:07 +0300

    more debugging prints, rotation off, bugfixes

--- pic glitch.py
+++ pic glitch.py
@@ -8,15 +8,16 @@ import sys
 import cv2
 import string
 
-in_dir = "D:\\Really Useful Files\\images\\mandel\\frames\\12_8_orig\\"           #input directory
-in_name = "new"               #input file name
-ext = ".jpg"                   #file extension
-out_dir = "D:\\Really Useful Files\\images\\mandel\\frames\\12_8\\"          #output directory
+in_dir = "D:\\Really Useful Files\\images\\mandel\\frames\\12_10_orig\\"           #input directory
+in_name = "orig"               #input file name
+ext = ".png"                   #file extension
+out_dir = "C:\\test\\"          #output directory
 out_name = "new000"                #output file name +index
 #files_amt = 10                  #number of files to be outputted
-bytes_amt = 2               #number of bytes to be modified per file
+bytes_amt = 1               #number of bytes to be modified per file
 #rotations = [180, 180]
 lo = "  "
+iterpass = 0
 
 def splitter(input_vid):
     os.chdir(in_dir)
@@ -49,39 +50,47 @@ def compensateRotation(out_path):
     print(lo + lo + "Compensation finished!")
 
 def verifyImg(in_path, out_path):
+    iterpass = 0
     try:
+        img = Image.open(out_path)
+        img.verify()
         img = Image.open(out_path) 
-        img.verify() 
         img.save(out_path)
         print(lo + "Verification passed!")
-    except (IOError, SyntaxError, OSError, UnidentifiedImageError, AttributeError) as e:
-        print(lo + "Bad image, error: " + str(e) + ", retrying glitch...")
-        os.remove(out_path)
-        print(lo + "File deleted!")
+        iterpass = 0
+    except (IOError, SyntaxError, OSError, UnidentifiedImageError, AttributeError, RecursionError) as e:
+
+        #print(lo + "[PASS{}] Bad image, error: \"{}\", removing broken file".format(iterpass,e))
         shutil.copyfile(in_path, out_path)
-        print(lo + "File replaced!")
+        #print(lo + "[PASS{}] File replaced, retrying glitch".format(iterpass))
+    
         glitch()
 
 def glitch():
-    rotateRandom(out_path)
+    #rotateRandom(out_path)
     fh = open(out_path, "r+b")
+    offsets = []
+    #iterpass += 1
     for j in range(bytes_amt):
         offset = random.randint(100, size) #offsetted to avoid corrupting the header, still breaks sometimes :(
         fh.seek(offset)      
-        #print(lo + lo + "Byte offset: " + str(offset))
-        randomStrTest = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(1))
-        print(lo + lo + "Random Str: " + randomStrTest)
-        randomByteValue = bytes(random.randint(0, 255))
+        #offsets.append(offset)
+        randomStr = ''.join(random.SystemRandom().choice(string.printable + string.digits) for _ in range(1))
+        randomByteValue = bytes(randomStr, 'ASCII')
         fh.write(randomByteValue)
-        #print("New Byte Value: " + str(randomByteValue))
-    #verifyImg(in_path, out_path)
-    compensateRotation(out_path)
+        #print("Edited byte @ offset: {}, [{:>5}], Value: {}".format('0x%0*X' % (5, offset), offset, '0x%0*X' % (2,ord(randomByteValue))))
+        #print("New Byte Value: " + str(hex(ord(randomByteValue)).zfill(2)).upper() + ", Random Str: " + randomStr)
     fh.close()
+    verifyImg(in_path, out_path)
+    #compensateRotation(out_path)
+
 
 start = time.time()
 path, dirs, files = next(os.walk(in_dir))
-files_amt = 40
+files_amt = 1000
+
 for x in range(1, files_amt + 1):
+    #iterpass = 0
     rrot = random.randint(0, 3) * 90
     print("=Started Rendering frame " + str(x) +"...=")
     trailing_0 = str(x).zfill(3)
@@ -96,7 +105,11 @@ for x in range(1, files_amt + 1):
     glitch()
     print("=Finished Rendering frame " + str(x) + ", " + str(files_amt - x) + " more to go!" + "=")
  
+
+ 
 end = time.time()
 print("=== Done! Elapsed time: " + str(end - start) + " seconds====")
+
+
 
 
