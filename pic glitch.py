c05e4b05481ee4d25ad4697b15146ea41db26e6c
artemetra <artemlukin15@gmail.com>
2021-06-03 01:03:20 +0300

    migrated config variables to YAML + code organisation

--- pic glitch.py
+++ pic glitch.py
@@ -7,17 +7,12 @@ import time
 import sys
 import cv2
 import string
+import yaml
 
-in_dir = "D:\\Really Useful Files\\images\\mandel\\frames\\12_10_orig\\"           #input directory
-in_name = "orig"               #input file name
-ext = ".jpg"                   #file extension
-out_dir = "C:\\test\\fjfj\\"          #output directory
-out_name = "new000"                #output file name +index
-#files_amt = 10                  #number of files to be outputted
-bytes_amt = 200               #number of bytes to be modified per file
-#rotations = [180, 180]
+#Internal flags and variables.
 lo = "  "
 _flag = True
+_rot = 0
 
 def splitter(input_vid):
     os.chdir(in_dir)
@@ -25,7 +20,7 @@ def splitter(input_vid):
     success,image = vidcap.read()
     count = 0
     while success:
-        cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+        cv2.imwrite("frame%d.jpg" % count, image)
         success,image = vidcap.read()
         #print('Read a new frame #' + str(count) + ': ' + str(success))
         count += 1
@@ -36,16 +31,16 @@ def splitter(input_vid):
 
 def rotateRandom(out_path):
     i = Image.open(out_path)
-    #rrot = random.randint(0, 1) * 180
-    print(lo + lo + "Rotating by " + str(rrot) + " degrees")
-    i = i.rotate(int(rrot), expand = 1)
+    _rot = random.randint(0, 1) * 180
+    print(lo + lo + "Rotating by " + str(_rot) + " degrees")
+    i = i.rotate(int(_rot), expand = 1)
     i.save(out_path)
     print(lo + lo + "Rotation applied!")
     
 def compensateRotation(out_path):
     i = Image.open(out_path)
-    i = i.rotate(360 - int(rrot), expand = 1)
-    print(lo + lo + "Compensating by " + str(360 - int(rrot)) + " degrees")
+    i = i.rotate(360 - int(_rot), expand = 1)
+    print(lo + lo + "Compensating by " + str(360 - int(_rot)) + " degrees")
     i.save(out_path)
     print(lo + lo + "Compensation finished!")
 
@@ -68,7 +63,6 @@ def glitch():
 
     fh = open(out_path, "r+b")
     offsets = []
-    #iterpass += 1
     for j in range(bytes_amt):
         offset = random.randint(100, size) #offsetted to avoid corrupting the header, still breaks sometimes :(
         fh.seek(offset)      
@@ -94,31 +88,47 @@ def manager(in_path, out_path):
     print(lo + lo + "Working image generated in {} passes".format(iterpass))
     #compensateRotation(out_path)
 
-start = time.time()
-path, dirs, files = next(os.walk(in_dir))
-files_amt = 1000
-
-for x in range(1, files_amt + 1):
-    #iterpass = 0
-    rrot = random.randint(0, 3) * 90
-    print("=Started Rendering frame " + str(x) +"...=")
-    trailing_0 = str(x).zfill(3)
-    in_path = in_dir + in_name + ext
-    print(lo + "In Path: " + in_path)
-    size = os.path.getsize(in_path)
-    #bytes_amt = size 
-    out_path = out_dir + out_name + trailing_0 + ext
-    print(lo + "Out Path: " + out_path)
-    shutil.copyfile(in_path, out_path)
-    print(lo + "File copied!")
-    manager(in_path, out_path)
-    print("=Finished Rendering frame " + str(x) + ", " + str(files_amt - x) + " more to go!" + "=")
- 
-
- 
-end = time.time()
-print("=== Done! Elapsed time: " + str(end - start) + " seconds====")
 
 
 
+def main():
+    start = time.time()
+    path, dirs, files = next(os.walk(in_dir))
+    files_amt = 1000
 
+    for x in range(1, files_amt + 1):
+        #iterpass = 0
+        _rot = random.randint(0, 3) * 90
+        print("=Started Rendering frame " + str(x) +"...=")
+        trailing_0 = str(x).zfill(3)
+        in_path = in_dir + in_name + ext
+        print(lo + "In Path: " + in_path)
+        size = os.path.getsize(in_path)
+        #bytes_amt = size 
+        out_path = out_dir + out_name + trailing_0 + ext
+        print(lo + "Out Path: " + out_path)
+        shutil.copyfile(in_path, out_path)
+        print(lo + "File copied!")
+        manager(in_path, out_path)
+        print("=Finished Rendering frame " + str(x) + ", " + str(files_amt - x) + " more to go!" + "=")
+    
+
+    
+    end = time.time()
+    print("=== Done! Elapsed time: " + str(end - start) + " seconds====")
+
+
+
+
+if __name__ == '__main__':
+    _conf = open(r"D:\\test\\glitcher\\config.yaml", 'r')
+    config = yaml.safe_load(_conf)
+    in_dir = config['in_dir']
+    in_name = config['in_name']
+    ext = config['ext']
+    out_dir = config['out_dir']
+    out_name = config['out_name']
+    files_amt = config['files_amt']
+    bytes_amt = config['bytes_amt']
+
+    main()
